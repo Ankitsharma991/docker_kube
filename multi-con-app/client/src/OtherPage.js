b3798cf076ad9b46e5401285b594704dbe222db2
Ankitsharma991 <sharmaa7861@gmail.com>
2024-03-26 01:20:30 +0530

    updating changes

---
+++ multi-con-app/client/src/OtherPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const OtherPage = () => {
+  return (
+    <div>
+      Im Some Other Page
+      <Link to="/">Go Back Home</Link>
+    </div>
+  );
+};
+
+export default OtherPage;
